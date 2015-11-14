15af670eb6c4fe0afbc702628bfa9c3666e12274
Jack O'Connor <oconnor663@gmail.com>
2015-11-14 17:58:35 -0500

    a little more

--- mpinc.py
+++ mpinc.py
@@ -17,34 +17,48 @@ long_types = [LongType(t) for t in [
 ]]
 
 
-def get_type(b):
+def get_type(buf):
     '''Return a type tuple and a size, or (None, None) to indicate that more
     bytes are required to determine the type/size.'''
     # Short circuit for empty bytes.
-    if len(b) == 0:
+    if len(buf) == 0:
         return None, None
     # Check to see if we have a short tag (just 1 byte).
     for t in short_types:
-        if (b[0] & t.tag_mask) == t.tag:
+        if (buf[0] & t.tag_mask) == t.tag:
             size_mask = 0b11111111 ^ t.tag_mask
-            size = b[0] & size_mask
+            size = buf[0] & size_mask
             return t, size
     # Check to see if we have a long tag, and if so whether we have the size.
     for t in long_types:
-        if b[0] == t.tag:
-            if len(b) >= t.size_bytes+1:
-                size = int.from_bytes(b[1:t.size_bytes+1], byteorder='big')
+        if buf[0] == t.tag:
+            if len(buf) >= t.size_bytes+1:
+                size = int.from_bytes(buf[1:t.size_bytes+1], byteorder='big')
                 return t, size
             else:
                 # We have the tag, but we need more bytes for the size.
                 return None, None
-    raise ValueError('No type recognized for ' + repr(b))
+    raise ValueError('No type recognized for ' + repr(buf))
 
 
 class Decoder:
     def __init__(self):
-        self.buffer = bytearray()
-        self.objects = []
+        self.has_value = False
+        self.value = None
+        self._buffer = bytearray()
+        self._type = None
+        self._size = 0
 
-    def write(self, b):
-        self.buffer.extend(b)
+    def write(self, buf):
+        '''Feed bytes into the decoder. Returns the number of bytes accepted,
+        which is zero if this decoder already has a value.'''
+        # If we don't have a type yet, try to derive one from the bytes we have
+        # so far plus the first 18 bytes of buf. (The largest possible
+        # MessagePack tag is fixext16, which is 18 bytes long.)
+        if self.type is None:
+            self._type, self._size = get_type(self._buffer + buf[:18])
+        # If we still don't have a type, accept all of buf and return.
+        if self.type is None:
+            self._buffer.extend(buf)
+            return len(buf)
+        # Now figure out how many total bytes we're supposed to need...
