ff4126db2ed45d8871bd24a189c93b55dca3f9d9
Jack O'Connor <oconnor663@gmail.com>
2015-11-17 01:55:51 -0500

    working

--- mpinc.py
+++ mpinc.py
@@ -2,6 +2,7 @@
 
 from collections import namedtuple
 from struct import pack
+import io
 
 
 # tag: The expected first byte when masked with tag_mask, e.g. 0xc0 for nil.
@@ -47,21 +48,19 @@ def get_N(mptype, tag_byte, N_buf):
     else:
         return tag_byte & mptype.N_mask
 
-def get_len(mptype, tag_byte, N_buf):
+def get_len(mptype, N):
     # The length is either fixed, or a function of N.
     if mptype.len is not None:
         return mptype.len
     else:
-        N = get_N(mptype, tag_byte, N_buf)
         return mptype.len_fn(N)
 
-def build(mptype, tag_byte, N_buf, buf_or_items):
+def build(mptype, N, buf_or_items):
     # The build_fn takes one of three arguments:
     #   1) N, if len is fixed at 0.
     #   2) a list of objects
     #   3) a bytearray
     if mptype.len == 0:
-        N = get_N(mptype, tag_byte, N_buf)
         return mptype.build_fn(N)
     expected_type = list if mptype.is_container else bytearray
     assert isinstance(buf_or_items, expected_type)
@@ -177,202 +176,91 @@ make_type(0xd5, "fixext2", build_ext, len=3)
 make_type(0xd6, "fixext4", build_ext, len=5)
 make_type(0xd7, "fixext8", build_ext, len=9)
 make_type(0xd8, "fixext16", build_ext, len=17)
-make_type(0xc7, "ext8", build_ext, len_fn=ext_len)
-make_type(0xc8, "ext16", build_ext, len_fn=ext_len)
-make_type(0xc9, "ext32", build_ext, len_fn=ext_len)
+make_type(0xc7, "ext8", build_ext, N_size=1, len_fn=ext_len)
+make_type(0xc8, "ext16", build_ext, N_size=2, len_fn=ext_len)
+make_type(0xc9, "ext32", build_ext, N_size=4, len_fn=ext_len)
 
 
 # --------- Decoder ---------------
-
-
-# TODO: Can we avoid using a big class for all this?
-def make_type(tag, name, builder_fn, size_len=0, tag_bits=8,
-              is_container=False, size_fn=lambda x: x):
-
-    tag_mask = (255 << (8-tag_bits)) % 256
-    size_mask = 255 ^ tag_mask
-
-    class Type:
-        @staticmethod
-        def matches_tag(byte):
-            if tag_mask is not None:
-                byte = tag_mask & byte
-            return byte == tag
-
-        def __init__(self, tag_byte):
-            self.tag_byte = tag_byte
-            self.size_len = size_len
-            self.name = name
-            self.is_container = is_container
-
-        def get_N(self, size_buffer):
-            assert len(size_buffer) == size_len
-            if size_len == 0:
-                # The size is stored in the tag byte. Mask it out.
-                return size_mask & self.tag_byte
-            else:
-                # TODO: map and ext types will behave differently here.
-                return int.from_bytes(size_buffer, byteorder='big')
-
-        def size(self, size_buffer):
-            N = self.get_N(size_buffer)
-            return size_fn(N)
-
-        def build(self, size_buffer, payload):
-            N = self.get_N(size_buffer)
-            return builder_fn(N, payload)
-
-    return Type
-
-
-def size_const(const):
-    return lambda N: const
-
-
-def size_map(N):
-    return 2*N
-
-
-def size_ext(N):
-    return N + 1
-
-
-def build_const(const):
-    return lambda N, payload: const
-
-
-def build_return_N(N, payload):
-    return N
-
-
-def build_bytes(N, payload):
-    return bytes(payload)
-
-
-def build_tuple(N, payload):
-    return tuple(payload)
-
-
-def build_int(N, payload):
-    return int.from_bytes(payload, signed=True, byteorder='big')
-
-
-def build_uint(N, payload):
-    return int.from_bytes(payload, signed=False, byteorder='big')
-
-
-def build_str(N, payload):
-    return payload.decode('utf8')
-
-
-def build_map(N, payload):
-    return {payload[2*i]: payload[2*i+1] for i in range(len(payload)//2)}
-
-
-def build_ext(N, payload):
-    return Ext(payload[0], bytes(payload[1:]))
-
-
-MessagePackTypes = [
-    make_type(0x00, "positive fixint", build_return_N, tag_bits=1,
-              size_fn=size_const(0)),
-    make_type(0x80, "fixmap", build_map, tag_bits=4, is_container=True,
-              size_fn=size_map),
-    make_type(0x90, "fixarray", build_tuple, tag_bits=4, is_container=True),
-    make_type(0xa0, "fixstr", build_str, tag_bits=3),
-    make_type(0xc0, "nil", build_const(None), size_fn=size_const(0)),
-    make_type(0xc2, "false", build_const(False), size_fn=size_const(0)),
-    make_type(0xc3, "true", build_const(True), size_fn=size_const(0)),
-    make_type(0xc4, "bin8", build_bytes, size_len=1),
-    make_type(0xc5, "bin16", build_bytes, size_len=2),
-    make_type(0xc6, "bin32", build_bytes, size_len=4),
-    make_type(0xc7, "ext8", build_ext, size_len=1, size_fn=size_ext),
-    make_type(0xc8, "ext16", build_ext, size_len=2, size_fn=size_ext),
-    make_type(0xc9, "ext32", build_ext, size_len=4, size_fn=size_ext),
-    make_type(0xdc, "array16", build_tuple, size_len=2, is_container=True),
-]
-
 
 class Decoder:
     def __init__(self):
         self.has_value = False
         self.value = None
-        self._size_buffer = bytearray()
         self._type = None
-        self._size = None
-        self._payload_buffer = bytearray()
-        self._payload_list = []
+        self._N_buf = bytearray()
+        self._N = None
+        self._L = None
+        self._value_buf = bytearray()
+        self._items = []
         self._child_decoder = None
 
-    def write(self, buf, buf_start=0):
-        '''Feed bytes into the decoder. Returns the number of bytes used. Once
-        an object has been parsed, no more bytes will be used.'''
-        if len(buf) == 0 or self.has_value:
-            return 0
-        used = 0
-        used += self._read_type(buf, buf_start)
-        used += self._try_read_size(buf, buf_start + used)
-        if self._size is None:
-            return used
+    def write(self, buf):
+        '''Wrap the input buffer in a BytesIO, feed it in, and return the
+        number of bytes used.'''
+        bytesio = io.BytesIO(buf)
+        self._write_bytesio(bytesio)
+        return bytesio.tell()
+
+    def _write_bytesio(self, bytesio):
+        if self.has_value:
+            return
+        self._read_type(bytesio)
+        if self._type is None:
+            return
+        self._read_N(bytesio)
+        if self._N is None:
+            return
         if self._type.is_container:
-            used += self._write_into_container(buf, buf_start + used)
+            self._read_into_container(bytesio)
         else:
-            used += self._write_into_bytes(buf, buf_start + used)
-        return used
+            self._read_into_bytes(bytesio)
 
-    def _read_type(self, buf, buf_start):
-        '''Set self._type, and return 1 if we consumed a byte to do that,
-        otherwise, 0.'''
+    def _read_type(self, bytesio):
         if self._type is not None:
-            return 0
-        tag_byte = buf[buf_start]
-        for T in MessagePackTypes:
-            if T.matches_tag(tag_byte):
-                self._type = T(tag_byte)
-                return 1
-        raise TypeError('unknown tag byte: ' + repr(tag_byte))
+            return
+        tag_byte = bytesio.read(1)
+        if tag_byte == b'':
+            return
+        self._tag_byte = tag_byte[0]
+        self._type = get_type(self._tag_byte)
 
-    def _try_read_size(self, buf, buf_start):
-        '''Read bytes into self._size_buffer until it's has all the size bytes
-        for the current type. We might not have enough to fill it completely.
-        (The caller will have to check self._size after this to see if we set
-        it.) Return the number of bytes we used.'''
-        if self._size is not None:
-            return 0
-        bytes_needed = self._type.size_len - len(self._size_buffer)
-        bytes_to_use = buf[buf_start:buf_start+bytes_needed]
-        self._size_buffer.extend(bytes_to_use)
-        if len(self._size_buffer) == self._type.size_len:
-            self._size = self._type.size(self._size_buffer)
-        return len(bytes_to_use)
+    def _read_N(self, bytesio):
+        if self._N is not None:
+            return
+        bytes_needed = self._type.N_size - len(self._N_buf)
+        bytes_read = bytesio.read(bytes_needed)
+        self._N_buf.extend(bytes_read)
+        if len(self._N_buf) == self._type.N_size:
+            self._N = get_N(self._type, self._tag_byte, self._N_buf)
+            self._L = get_len(self._type, self._N)
 
-    def _write_into_container(self, buf, buf_start):
-        used = 0
-        while len(self._payload_list) < self._size:
-            if len(buf) <= buf_start + used:
+    def _read_into_container(self, bytesio):
+        if self.has_value:
+            return
+        while len(self._items) < self._L:
+            if len(bytesio.getvalue()) <= bytesio.tell():
                 break
             if self._child_decoder is None:
                 self._child_decoder = Decoder()
-            used += self._child_decoder.write(buf, buf_start + used)
+            self._child_decoder._write_bytesio(bytesio)
             if self._child_decoder.has_value:
-                self._payload_list.append(self._child_decoder.value)
+                self._items.append(self._child_decoder.value)
                 self._child_decoder = None
         else:
             self.has_value = True
-            self.value = self._type.build(
-                self._size_buffer, self._payload_list)
-        return used
+            self.value = build(self._type, self._N, self._items)
 
-    def _write_into_bytes(self, buf, buf_start):
+    def _read_into_bytes(self, bytesio):
+        if self.has_value:
+            return
         # TODO: Some duplicated code here.
-        bytes_needed = self._size - len(self._payload_buffer)
-        bytes_to_use = buf[buf_start:buf_start+bytes_needed]
-        self._payload_buffer.extend(bytes_to_use)
-        if len(self._payload_buffer) == self._size:
+        bytes_needed = self._L - len(self._value_buf)
+        bytes_read = bytesio.read(bytes_needed)
+        self._value_buf.extend(bytes_read)
+        if len(self._value_buf) == self._L:
             self.has_value = True
-            self.value = self._type.build(
-                self._size_buffer, self._payload_buffer)
-        return len(bytes_to_use)
+            self.value = build(self._type, self._N, self._value_buf)
 
 
 def main():
@@ -383,11 +271,11 @@ def main():
         1,
         "",
         "foo",
-        (),
-        (1, "two", 3),
-        (0) * 100,
+        [],
+        [1, "two", 3],
+        [0] * 100,
         {},
-        {'a': 1, 'b': (2,)},
+        {'a': 1, 'b': [2]},
         None,
         True,
         False,
@@ -405,8 +293,8 @@ def main():
         used = d.write(b)
         assert d.has_value
         new_val = d.value
-        if isinstance(new_val, Ext):
-            new_val = umsgpack.Ext(*new_val)
+        if isinstance(val, umsgpack.Ext):
+            val = Ext(val.type, val.data)
         assert val == new_val, '{} != {}'.format(repr(val), repr(new_val))
         assert used == len(b), 'only used {} bytes out of {}'.format(len(b), b)
 
