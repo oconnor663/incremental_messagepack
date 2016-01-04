88480e4eeb2ce86e4c6be725eaaabc90f461cb2a
Jack O'Connor <oconnor663@gmail.com>
2016-01-04 09:43:21 -0500

    decoder_with_generators.py

---
+++ decoder_with_generators.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+
+# The main decoder.py implementation doesn't use generators, to make it easy to
+# translate into languages that don't have them. However, generators are a
+# wonderful way to represent interruptible functions that can pick up where
+# they left off. This is an alternative implementation of the same incremental
+# decoder using generators.
+
+import io
+
+# Reuse definitions from the first decoder.
+import decoder
+
+class DelegatableBuffer:
+    def __init__(self, size):
+        self.buffer = bytearray()
+        self.size = size
+
+    def fill(self, bytesio=io.BytesIO()):
+        '''The contract of the fill() generator is this:
+        1) The caller calls fill() with an optional initial BytesIO reader.
+        2) The caller begins the generator with next().
+        3) Until StopIteration is raised, the caller uses send() to pass in
+           further BytesIO readers.
+        4) When the caller catches StopIteration, the buffer member is full.
+           The attached value on the StopIteration will be the last BytesIO
+           reader passed in.
+
+        This contract is designed so that another generator with the same
+        contract may delegate to this one with `yield from`.
+        '''
+        while True:
+            needed = self.size - len(self.buffer)
+            bytes_received = bytesio.read(needed)
+            self.buffer.extend(bytes_received)
+            if len(bytes_received) < needed:
+                bytesio = yield
+            else:
+                return bytesio
+
+class MessagePackDecoder:
+    def __init__(self):
+        self._full = False
+        self._result = None
+        self._generator = self.fill()
+        next(self._generator)
+
+    def full(self):
+        return self._full
+
+    def result(self):
+        return self._result
+
+    def write(self, buf):
+        bytesio = io.BytesIO(buf)
+        try:
+            self._generator.send(bytesio)
+        except StopIteration:
+            pass
+        return bytesio.tell()
+
+    def fill(self, bytesio=io.BytesIO()):
+        tag_buf = DelegatableBuffer(1)
+        bytesio = yield from tag_buf.fill(bytesio)
+        tag_byte = tag_buf.buffer[0]
+        format = decoder.get_format(tag_byte)
+        N_buf = DelegatableBuffer(format.N_size)
+        bytesio = yield from N_buf.fill(bytesio)
+        N, L = format.get_N_and_L(tag_byte, N_buf.buffer)
+        if format.holds_objects:
+            payload_list = []
+            while len(payload_list) < L:
+                subdecoder = MessagePackDecoder()
+                bytesio = yield from subdecoder.fill(bytesio)
+                payload_list.append(subdecoder.result())
+            self._result = format.build(N, payload_list)
+        else:
+            payload_buf = DelegatableBuffer(L)
+            bytesio = yield from payload_buf.fill(bytesio)
+            self._result = format.build(N, payload_buf.buffer)
+        self._full = True
+        return bytesio
+
+def main():
+    for b, val in decoder.tests:
+        d = MessagePackDecoder()
+        used = d.write(b)
+        assert d.full()
+        assert val == d.result(), '{} != {}'.format(
+            repr(val), repr(d.result()))
+        assert used == len(b), 'only used {} bytes out of {}'.format(len(b), b)
+        # Do it again one byte at a time.
+        d = MessagePackDecoder()
+        for i in range(len(b)):
+            d.write(b[i:i+1])
+        assert d.full()
+        assert val == d.result(), '{} != {}'.format(
+            repr(val), repr(d.result()))
+
+if __name__ == '__main__':
+    main()
